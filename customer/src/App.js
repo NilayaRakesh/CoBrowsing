c1dc05f131309f3b6087198cf3270212183f67e2
Pawan Kumar Dubey <spinifer_x@Pawans-MacBook-Pro.local>
2022-07-01 09:50:35 +0530

    client frontend

---
+++ customer/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AccountPage from './accountPage';
+
+function App() {
+  return (
+    <div>
+      <AccountPage></AccountPage>
+    </div>
+  );
+}
+
+export default App;
