81a9e6e7175278135983a731e468fea7f43d7860
Pawan Kumar Dubey <spinifer_x@Pawans-MacBook-Pro.local>
2022-07-01 19:27:24 +0530

    full code

--- customer/src/serverManager.jsx
+++ customer/src/serverManager.jsx
@@ -1,15 +1,16 @@
 import domtoimage from 'dom-to-image';
 import { io } from "socket.io-client";
-import robot from 'robotjs';
+import {saveAs} from "file-saver";
+//import robot from 'robotjs';
 var interval;
-const socket = io("https:localhost:5000");
+const socket = io("http://localhost:5001");
 export const ipcStart = () => {
     console.log(" Screen Share started ");
 
     interval = setInterval(function() {
         domtoimage.toBlob(document.getElementById('my-node'))
         .then(function (blob) {
-            window.saveAs(blob, 'my-node.png');
+           // window.saveAs(blob, 'my-node.png');
             console.log("blob blob->",blob);
             let base64Stream;
             var reader = new FileReader();
@@ -18,22 +19,28 @@ export const ipcStart = () => {
             var base64data = reader.result;  
             base64Stream = base64data;              
             console.log(base64data);
+            const data = {
+                "roomId": "123",
+                "base64": base64Stream
             }
-
-           socket.emit("screen-data", JSON.stringify(base64Stream));
+           socket.emit("shareScreen","12345",base64data);
+            }
+           // console.log("base 64 ->",base64Stream);
+           
         });
      },1000);
 
-     socket.on("mouse-move", function(data){
+     socket.on("mouseMovement", (data) => {
         var obj = JSON.parse(data);
         var x = obj.x;
         var y = obj.y;
-
-        robot.moveMouse(x, y);
+        console.log("mouse moved");
+        //robot.moveMouse(x, y);
     })
 
-    socket.on("mouse-click", function(data){
-        robot.mouseClick();
+    socket.on("mouseClick", (data) => {
+       // robot.mouseClick();
+       console.log("mouse clicked");
     })
 }
 
