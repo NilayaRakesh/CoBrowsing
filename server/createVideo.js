fdde816a3a268ab76a4d7cd0d051f3a1183dbad0
yashmustak <srivastavayash1234@gmail.com>
2022-07-01 16:54:50 +0530

    updated python script

--- server/createVideo.js
+++ server/createVideo.js
@@ -3,6 +3,6 @@ const spawn = require("child_process").spawn;
 module.exports = {
     createVideo: function(room){
         console.log("spawning python script");
-        spawn('python3',["s3_to_video.py", room]);
+        spawn('python3',["scripts/s3_to_video.py", room]);
     }
 }
